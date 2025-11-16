1a91ee397fb57e7255706aeb9c969b6b1a5cf9df
lwozniak <lukasz.wozniak@expert4you.pl>
2025-11-16 18:22:24 +0100

    feat: implement AI flashcard generation, management, and deletion flows

--- assets/app.js
+++ assets/app.js
@@ -1,3 +1,13 @@
+// import './stimulus_bootstrap.js';
+// /*
+//  * Welcome to your app's main JavaScript file!
+//  *
+//  * This file will be included onto the page via the importmap() Twig function,
+//  * which should already be in your base.html.twig.
+//  */
+// import './styles/app.css';
+
+
 import './stimulus_bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
@@ -6,3 +16,5 @@ import './stimulus_bootstrap.js';
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
